5a940e29bff317be4fc270093fb09877ea347b92
Gia Edgington <gia.edgington@gmail.com>
2020-04-06 12:25:08 -0400

    Add to cart functionality

--- controllers/shop.js
+++ controllers/shop.js
@@ -62,7 +62,15 @@ exports.getCart = (req, res, nect) => {
 
 exports.postCart = (req, res, next) => {
     const prodId = req.body.productId;
-    let fetchedCart;
+    Product.findById(prodId)
+    .then(product => {
+        return req.user.addToCart(product);
+    })
+    .then(result => {
+        console.log(result);
+    })
+
+/*     let fetchedCart;
     let newQuantity = 1;
     req.user
     .getCart()
@@ -91,7 +99,7 @@ exports.postCart = (req, res, next) => {
     .then(() => {
         res.redirect('/cart');
     })
-    .catch(err => console.log(err));
+    .catch(err => console.log(err)); */
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
